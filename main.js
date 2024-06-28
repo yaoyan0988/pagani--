b77635ba7e6acc095673eb0d59ae1cf49a9a1e70
yaoyan0988 <night840121@gmail.com>
2024-06-29 00:00:55 +0800

    彈出視窗更新

--- main.js
+++ main.js
@@ -14,10 +14,26 @@ const navBar = document.querySelector("nav"),
     $(document).ready(function() {
       Swal.fire({
         imageUrl: "img/新聞稿-02.png",
+        showCloseButton: true,
         height: 1700,
         width:1200,
-        imageAlt: "A tall image"
+        confirmButtonText: `close`,
+        confirmButtonColor: "#6df3f6",
       });
+      // Swal.fire({
+        
+      //   showCloseButton: true,
+      //   // showCancelButton: true,
+      //   focusConfirm: false,
+      //   confirmButtonText: `
+      //     <i class="fa fa-thumbs-up"></i> Great!
+      //   `,
+      //   confirmButtonAriaLabel: "Thumbs up, great!",
+      //   cancelButtonText: `
+      //     <i class="fa fa-thumbs-down"></i>
+      //   `,
+      //   cancelButtonAriaLabel: "Thumbs down"
+      // });
     });
 
     
